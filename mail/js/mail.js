60a04d9decad359449de93f1604df03fbbb38bde
Nicole Mastrostefano <nicole_mas@hotmail.it>
2020-11-12 18:24:00 +0100

    mail ok

--- mail/js/mail.js
+++ mail/js/mail.js
@@ -1,17 +1,25 @@
 // Chiedi all’utente la sua email
 var utente = (prompt("inserisci la tua e-mail"));
 console.log(utente);
-// controlla che sia nella lista di chi può accedere
+
 
 var mail = ["diego@gmail.com", "nicole@gmail.com","ludovica@gmail.com"]
 console.log(mail);
 
+var esito = false;
+// controlla che sia nella lista di chi può accedere
+  for (var i = 0; i < mail.length; i++) {
+  console.log(mail[i])
+  
+    if (utente == mail[i]) {
+    esito = true;
+    console.log(esito);
+    }
+  }
 
-
-    for (var i = 0; i < mail.length; i++) {
-    console.log(mail[i]);
-
-    if (mail[i] === utente) {
+  if (esito == true) {
       document.getElementById("messaggio").innerHTML = "la tua mail è stata trovata";
-      }
+  }
+    else {
+      document.getElementById("messaggio").innerHTML = "la tua mail non è stata trovata";
     }
