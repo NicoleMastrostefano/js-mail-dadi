838942462d7560ae89fed2cea3dd7eb0da153954
Nicole Mastrostefano <nicole_mas@hotmail.it>
2020-11-12 15:43:49 +0100

    dadi first commit

---
+++ js/dadi.js
@@ -0,0 +1,18 @@
+// Generare un numero random da 1  a 6 per il computer
+function casuale() {
+var n1=Math.round(Math.random()*5+1);
+console.log(n1);
+document.getElementById("numero1").innerHTML = "Hai ottenuto " + n1;
+
+// Generare un numero random da 1  a 6 per il computer
+var n2=Math.round(Math.random()*5+1);
+console.log(n2);
+document.getElementById("numero2").innerHTML = "L'avversario ha ottenuto " + n2;
+
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+if (n1 > n2 )
+    document.getElementById("messaggio").innerHTML = "HAI VINTO!";
+
+else
+    document.getElementById("messaggio").innerHTML = "HAI PERSO!";
+}
